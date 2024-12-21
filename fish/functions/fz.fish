c2c87b64f59505814208952b5603d5e7791d8393
Shane Stephenson <stephenson.shane.a@gmail.com>
2024-12-21 17:52:23 -0500

    updated a bit

--- fish/functions/fz.fish
+++
@@ -1,7 +0,0 @@
-function fz -d "default fuzzy finder based on current system"
-    if type -q "sk"
-        sk --bind 'ctrl-l:toggle+out,ctrl-h:toggle+in,ctrl-space:toggle' $argv
-    else if type -q "fzf"
-        fzf --bind 'ctrl-l:toggle+out,ctrl-h:toggle+in,ctrl-space:toggle' $argv
-    end
-end
