809f43de4ba89e978a574a00e6bb3305e23187c5
Shane Stephenson <stephenson.shane.a@gmail.com>
2025-02-01 17:06:46 -0500

    dump cmd

--- fish/locals/default.fish
+++ fish/locals/default.fish
@@ -1,3 +1,4 @@
 set -gx EDITOR nvim
 set -gx SHELL fish
-set -gx PYTHON_VENV_DIR ~/.local/python-venvs
+set -gx PYTHON_VENV_DIR $HOME/.local/python-venvs
+set -gx CLI_NOTES $HOME/.config/notes/cmd_examples.txt
